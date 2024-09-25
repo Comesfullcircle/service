fbb3cd0200f54cef62b82af6752a603f5a35182e
윤설이 <olerlmin1234@gmail.com>
2024-09-25 15:17:03 +0900

    배달플랫폼 서비스 개발 - 멀티모듈 초기 설정

---
+++ api/src/main/java/org/delivery/Main.java
@@ -0,0 +1,13 @@
+package org.delivery;
+
+import org.delivery.db.UserDto;
+
+public class Main {
+    //public static void main(String[] args) {
+   //     System.out.println("Hello world!");
+    //}
+    public static void main(String args[]){
+        UserDto userDto = new UserDto();
+    }
+
+}
