8cd6142de0a8c9540b42e76ae2be103d28813a32
윤설이 <olerlmin1234@gmail.com>
2024-10-01 01:29:53 +0900

    사용자 주문 로직 생성

---
+++ api/src/main/java/org/delivery/api/domain/userorder/controller/model/UserOrderRequest.java
@@ -0,0 +1,25 @@
+package org.delivery.api.domain.userorder.controller.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserOrderRequest {
+
+    // 주문
+    // 특정 사용자가, 특정 메뉴를 주문
+    // 특정 사용자 = 로그인된 세션에 들어있는 사용자
+    // 특정 메뉴 id
+
+    @JsonProperty("storeMenuIdList")
+    @NotNull
+    private List<Long> storeMenuIdList;
+
+}
