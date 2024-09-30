d28ce3107b32bd9db264927ed9bc0ad201272e2f
윤설이 <olerlmin1234@gmail.com>
2024-09-30 10:13:14 +0900

    user_order, user_order_menu db 생성 -> 엔티티 생성

---
+++ db/src/main/java/org/delivery/db/userorder/enums/UserOrderStatus.java
@@ -0,0 +1,16 @@
+package org.delivery.db.userorder.enums;
+
+import lombok.AllArgsConstructor;
+
+public enum UserOrderStatus {
+
+    REGISTERED("등록"),
+    UNREGISTERED("해지")
+    ;
+
+    UserOrderStatus(String description) {
+        this.description = description;
+    }
+
+    private String description;
+}
