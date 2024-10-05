eb4a3e9806d7c728fdd66c21bbefc8aca293f706
윤설이 <olerlmin1234@gmail.com>
2024-10-05 17:53:36 +0900

    UserSession 값 들어오는 것 확인

--- store-admin/src/main/java/org/delivery/storeadmin/presentation/PageController.java
+++ store-admin/src/main/java/org/delivery/storeadmin/presentation/PageController.java
@@ -1,8 +1,13 @@
 package org.delivery.storeadmin.presentation;
 
+import org.delivery.storeadmin.domain.authorization.model.UserSession;
+import org.delivery.storeadmin.domain.user.controller.model.StoreUserResponse;
+import org.delivery.storeadmin.domain.user.converter.StoreUserConverter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -10,18 +15,32 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("")
 public class PageController {
 
-    @RequestMapping(path = {"", "/main"})
-    public ModelAndView main() {
+    @GetMapping(path = {"", "/main"})
+    public String main(Model model) {
         // SecurityContextHolder를 통해 Authentication 객체 가져오기
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null) {
+        if (authentication != null && authentication.getPrincipal() instanceof UserSession) {
+            UserSession userSession = (UserSession) authentication.getPrincipal();
+
+            // 모델에 UserSession 필드 추가하여 Thymeleaf로 전달
+            model.addAttribute("userId", userSession.getUserId());
+            model.addAttribute("email", userSession.getEmail());
+            model.addAttribute("status", userSession.getStatus());
+            model.addAttribute("role", userSession.getRole());
+            model.addAttribute("registeredAt", userSession.getRegisteredAt());
+            model.addAttribute("unregisteredAt", userSession.getUnregisteredAt());
+            model.addAttribute("lastLoginAt", userSession.getLastLoginAt());
+            model.addAttribute("storeId", userSession.getStoreId());
+            model.addAttribute("storeName", userSession.getStoreName());
+
             System.out.println("Authentication name: " + authentication.getName());
             System.out.println("Authorities: " + authentication.getAuthorities());
-            System.out.println("Principal: " + authentication.getPrincipal());
+            System.out.println("Principal: " + userSession);
         }
 
-        return new ModelAndView("main");
+        return "main"; // "main.html" 템플릿 이름
     }
+
 
     @RequestMapping("/order")
     public ModelAndView order() {
