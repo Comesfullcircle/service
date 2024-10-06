ff91592516e855e405159b42911fb650adf0d959
윤설이 <olerlmin1234@gmail.com>
2024-10-06 20:48:13 +0900

    SSE 통신 방식으로 사용자 주문 - api/user-order 생성 ( 사용자 user id 가 store_id , store_menu 활용 주문 로직 생성)

--- store-admin/src/main/java/org/delivery/storeadmin/domain/userorder/controller/model/UserOrderResponse.java
+++ store-admin/src/main/java/org/delivery/storeadmin/domain/userorder/controller/model/UserOrderResponse.java
@@ -1,12 +1,9 @@
-package org.delivery.db.userorder;
+package org.delivery.storeadmin.domain.userorder.controller.model;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
-import org.delivery.db.BaseEntity;
 import org.delivery.db.userorder.enums.UserOrderStatus;
 
 import java.math.BigDecimal;
@@ -15,23 +12,16 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(callSuper = false)
-@SuperBuilder
-@Entity
-@Table(name = "user_order")
-public class UserOrderEntity extends BaseEntity {
+@Builder
+public class UserOrderResponse {
+    private Long id;
 
-    @Column(nullable = false)
-    private Long userId; // user table 1:n
-
-    @Column(nullable = false)
     private Long storeId;
 
-    @Enumerated(EnumType.STRING)
-    @Column(length = 50, nullable = false)
+    private Long userId;
+
     private UserOrderStatus status;
 
-    @Column(precision = 11, scale = 4, nullable = false)
     private BigDecimal amount;
 
     private LocalDateTime orderedAt;
