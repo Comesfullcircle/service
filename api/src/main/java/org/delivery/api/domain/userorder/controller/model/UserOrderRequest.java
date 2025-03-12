328962f61b3d2346f369805c7674a0a2956163d7
윤설이 <olerlmin1234@gmail.com>
2025-03-12 20:14:11 +0900

    kotlin으로 변경 -> 비즈니스 로직 변경

--- api/src/main/java/org/delivery/api/domain/userorder/controller/model/UserOrderRequest.java
+++ api/src/main/java/org/delivery/api/domain/userorder/controller/model/UserOrderRequest.java
@@ -1,15 +1,15 @@
 package org.delivery.api.domain.userorder.controller.model;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.List;
 
-@Data
+//@Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class UserOrderRequest {
 
     @NotNull
@@ -22,4 +22,19 @@ public class UserOrderRequest {
     @NotNull
     private List<Long> storeMenuIdList;
 
+    public @NotNull Long getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(@NotNull Long storeId) {
+        this.storeId = storeId;
+    }
+
+    public @NotNull List<Long> getStoreMenuIdList() {
+        return storeMenuIdList;
+    }
+
+    public void setStoreMenuIdList(@NotNull List<Long> storeMenuIdList) {
+        this.storeMenuIdList = storeMenuIdList;
+    }
 }
