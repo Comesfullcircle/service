d5f463f000933babcf8ba385a22213a7c06b52c3
윤설이 <olerlmin1234@gmail.com>
2025-03-16 23:58:33 +0900

    모니터링 - Spring Boot Application 활용(OFFLINE에러 확인 하구 수정 ing)

--- api/src/main/kotlin/org/delivery/api/config/web/WebConfig.kt
+++ api/src/main/kotlin/org/delivery/api/config/web/WebConfig.kt
@@ -9,9 +9,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebConfig(
-    private val authorationInterceptor: AuthorizationInterceptor,
+    private val authorizationInterceptor: AuthorizationInterceptor,
     private val userSessionResolver: UserSessionResolver
-) : WebMvcConfigurer {
+) : WebMvcConfigurer{
+
 
     private val OPEN_API = listOf(
         "/open-api/**"
@@ -20,7 +21,8 @@ class WebConfig(
     private val DEFAULT_EXCLUDE = listOf(
         "/",
         "favicon.ico",
-        "/error"
+        "/error",
+        "/actuator/**"
     )
 
     private val SWAGGER = listOf(
@@ -30,7 +32,7 @@ class WebConfig(
     )
 
     override fun addInterceptors(registry: InterceptorRegistry) {
-        registry.addInterceptor(authorationInterceptor)
+        registry.addInterceptor(authorizationInterceptor)
             .excludePathPatterns(OPEN_API)
             .excludePathPatterns(DEFAULT_EXCLUDE)
             .excludePathPatterns(SWAGGER)
